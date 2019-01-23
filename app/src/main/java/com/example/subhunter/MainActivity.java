6db8ec65ed7a9ca00fd50871351c30d366eedd26
TheNeoCoder <the.neocoder@gmail.com>
2019-01-22 23:06:43 -0500

    Comments added according to the book. The comments follow a flow diagram provided in the book describing the flow of the game. Start, initialize, draw, handle input, update, handle input, restart ect...

--- app/src/main/java/com/example/subhunter/MainActivity.java
+++ app/src/main/java/com/example/subhunter/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.subhunter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    // Android runs this code just before the player sees the app. This makes it a good place to add
+    // the code for the 'one-time' setup phase.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
+
+    // This code will execute when a new game needs to be started. It will happen when the app is
+    // first started and after the player wins the game.
+
+
+    // Here we will do all the drawing. The grid lines, the HUD and the touch indicator.
+
+
+    // This part of the code will handle detecting that the player has tapped the screen.
+
+
+    // The code here will execute when the player taps the screen. It will calculate the distance
+    // from the sub and decide if there was a hit or miss.
+
 }
